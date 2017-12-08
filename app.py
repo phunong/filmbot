56341a44dc9f6ed2b6e85b403ea27ce315fe646a
PhuNong <32190177+phunong@users.noreply.github.com>
2017-12-08 15:37:48 +0700

    test

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     filmname = parameters.get("film_name")
-
-    film_info={'Diep Vien Bao Thu':'Link: http://www.phimmoi.net/phim/diep-vien-bao-thu-i3-5741/'}
-    film_info2={'Diep Vien Bao Thu':'Time: 114 minutes'}
-    film_info3={'Diep Vien Bao Thu':'Quality: HD'}
-    speech = "Thông Tin Phim:"+ "\nTên Phim:\t" + filmname + "\t\nLink:\t" + str(film_info[filmname]) + "\t\nThời Lượng:\t"+ str(film_info2[filmname])+"\t\nChất Lượng:\t"+ str(film_info3[filmname])
+    speech ="Thông Tin Phim" +"\nTên Phim" +filmname
+#     film_info={'Diep Vien Bao Thu':'Link: http://www.phimmoi.net/phim/diep-vien-bao-thu-i3-5741/'}
+#     film_info2={'Diep Vien Bao Thu':'Time: 114 minutes'}
+#     film_info3={'Diep Vien Bao Thu':'Quality: HD'}
+#     speech = "Thông Tin Phim:"+ "\nTên Phim:\t" + filmname + "\t\nLink:\t" + str(film_info[filmname]) + "\t\nThời Lượng:\t"+ str(film_info2[filmname])+"\t\nChất Lượng:\t"+ str(film_info3[filmname])
     print("Response:")
     print(speech)
     return {
