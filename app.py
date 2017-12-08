719ccaf65c7f958a001520305a56a5e9c4d5848b
PhuNong <32190177+phunong@users.noreply.github.com>
2017-12-08 14:57:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def makeWebhookResult(req):
     film_info={'Diep Vien Bao Thu':'Link: http://www.phimmoi.net/phim/diep-vien-bao-thu-i3-5741/'}
     film_info2={'Diep Vien Bao Thu':'Time: 114 minutes'}
     film_info3={'Diep Vien Bao Thu':'Quality: HD'}
-    speech = "Thông Tin Phim:"+"\t\n Tên Phim:\t" + filmname + "\t\nLink:\t" + str(film_info[filmname]) + "\t\nThời Lượng:\t"+ str(film_info2[filmname])+"\t\nChất Lượng:\t"+ str(film_info3[filmname])
+    speech = "Thông Tin Phim:"+ "\nTên Phim:\t" + filmname + "\t\nLink:\t" + str(film_info[filmname]) + "\t\nThời Lượng:\t"+ str(film_info2[filmname])+"\t\nChất Lượng:\t"+ str(film_info3[filmname])
     print("Response:")
     print(speech)
     return {
